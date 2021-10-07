fe5dbd80faf2a46a5f8593cf3cbd711d1cbb9736
Safwan Shaheer <devorein00@gmail.com>
2021-10-07 17:12:21 +0600

    Format menu and functions script file using prettier

--- js/functions.js
+++ js/functions.js
@@ -1,4 +1,5 @@
-const colors = ['#FDD79F',
+const colors = [
+	'#FDD79F',
 	'#F5F5F5',
 	'#797A7D',
 	'#3B6A6C',
@@ -6,28 +7,28 @@ const colors = ['#FDD79F',
 	'#2589BD',
 	'#FCB040',
 	'#6A8E8F',
-	'#C7C7C7'];
+	'#C7C7C7',
+];
 
 const allPaths = document.querySelectorAll('span.circle');
 
 function reloadColors(elem) {
 	allPaths.forEach(function recolor(elem) {
-	elem.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)];
-});
+		elem.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)];
+	});
 }
 
 function resetSpeed(elem) {
 	allPaths.forEach(function speed(elem) {
-			elem.style.animationDuration  = (Math.random() * (2 - 1) + 1) + "s";
-			elem.style.animationDelay   = (Math.random() * (1 - 0) + 0) + "s";
-		
+		elem.style.animationDuration = Math.random() * (2 - 1) + 1 + 's';
+		elem.style.animationDelay = Math.random() * (1 - 0) + 0 + 's';
 	});
 }
 
 function stopMovement(elem) {
-	clearInterval(autoReload)
+	clearInterval(autoReload);
 	allPaths.forEach(function speed(elem) {
-		elem.style.animation  = "none";
+		elem.style.animation = 'none';
 	});
 }
 
@@ -36,66 +37,67 @@ resetSpeed();
 
 const randInterval = Math.floor(Math.random() * 3500) + 800;
 
-console.log("Reloading every " + randInterval / 1000 + " seconds");
+console.log('Reloading every ' + randInterval / 1000 + ' seconds');
 
 autoReload = setInterval(reloadColors, randInterval);
 
-const formElement = document.querySelector("#form");
-const formButtonElement = document.querySelector(".form-button");
+const formElement = document.querySelector('#form');
+const formButtonElement = document.querySelector('.form-button');
 const inputElements = Array.from(formElement.querySelectorAll('input'));
 const textAreaElement = formElement.querySelector('textarea');
-const inputErrorElements = Array.from(document.querySelectorAll(".input-error"));
+const inputErrorElements = Array.from(document.querySelectorAll('.input-error'));
 const inputErrorElementsRecord = {};
 
-inputErrorElements.forEach(inputErrorElement=>inputErrorElementsRecord[inputErrorElement.getAttribute("name")] = inputErrorElement);
+inputErrorElements.forEach(
+	(inputErrorElement) =>
+		(inputErrorElementsRecord[inputErrorElement.getAttribute('name')] = inputErrorElement)
+);
 
 formButtonElement.onclick = async (e) => {
-  e.preventDefault();
-  const formData = {};
-  
-  [...inputElements, textAreaElement].forEach(inputElement=>{
-    const name = inputElement.name, value = inputElement.value;
-    if(!value) {
-      const inputErrorElement = inputErrorElementsRecord[name];
-      inputErrorElement.classList.add('opacity-1');
-    } else {
-      formData[name] = value;
-    }
-  });
+	e.preventDefault();
+	const formData = {};
 
-  if (formData.message && formData.company_name && formData.name && formData.email) {
-    const googleCaptcha = window.grecaptcha;
-    if (googleCaptcha) {
-      googleCaptcha.ready(async function () {
-        const token = await googleCaptcha.execute("6Lfplq4cAAAAADgQZAndUa11l-PJXBATMeZhgTEe", { action: 'submit' });
-        try {
-          const response = await fetch(`https://post-contact-worker.devorein.workers.dev/`, {
-            method: 'POST',
-            body: JSON.stringify({...formData, captchaToken: token}),
-            headers: {
-              'Content-Type': 'application/json'
-            }
-          });
-          const data = await response.json();
-          [...inputElements, textAreaElement].forEach(element => {
-            const inputErrorElement = inputErrorElementsRecord[element.name];
-            element.value = '';
-            inputErrorElement.classList.remove('opacity-1')
-          });
-          Swal.fire(
-            'Thank you!',
-            'Your email was sent successfully',
-            'success'
-          )
-        } catch (err) {
-          Swal.fire({
-            icon: 'error',
-            title: 'Oops...',
-            text: err.message,
-          })
-          console.log(err.message)
-        }
-      });
-    }
-  }
-}
+	[...inputElements, textAreaElement].forEach((inputElement) => {
+		const name = inputElement.name,
+			value = inputElement.value;
+		if (!value) {
+			const inputErrorElement = inputErrorElementsRecord[name];
+			inputErrorElement.classList.add('opacity-1');
+		} else {
+			formData[name] = value;
+		}
+	});
+
+	if (formData.message && formData.company_name && formData.name && formData.email) {
+		const googleCaptcha = window.grecaptcha;
+		if (googleCaptcha) {
+			googleCaptcha.ready(async function () {
+				const token = await googleCaptcha.execute('6Lfplq4cAAAAADgQZAndUa11l-PJXBATMeZhgTEe', {
+					action: 'submit',
+				});
+				try {
+					await fetch(`https://post-contact-worker.devorein.workers.dev/`, {
+						method: 'POST',
+						body: JSON.stringify({ ...formData, captchaToken: token }),
+						headers: {
+							'Content-Type': 'application/json',
+						},
+					});
+					[...inputElements, textAreaElement].forEach((element) => {
+						const inputErrorElement = inputErrorElementsRecord[element.name];
+						element.value = '';
+						inputErrorElement.classList.remove('opacity-1');
+					});
+					Swal.fire('Thank you!', 'Your email was sent successfully', 'success');
+				} catch (err) {
+					Swal.fire({
+						icon: 'error',
+						title: 'Oops...',
+						text: err.message,
+					});
+					console.log(err.message);
+				}
+			});
+		}
+	}
+};
--- js/menu.js
+++ js/menu.js
@@ -1,110 +1,108 @@
 const activeNavItem = document.querySelector('.nav-link');
 
-if(activeNavItem) {
-  activeNavItem.onClick = function(){
-    activeNavItem.classList.remove('active');
-    activeNavItem.classList.add('active');
-  };
+if (activeNavItem) {
+	activeNavItem.onClick = function () {
+		activeNavItem.classList.remove('active');
+		activeNavItem.classList.add('active');
+	};
 }
 
-function onClickMenu(){
-	document.getElementById("menu").classList.toggle("change");
-	document.getElementById("nav").classList.toggle("change");
-	document.getElementById("menu-bg").classList.toggle("change-bg");
-	document.getElementById("content").classList.toggle("blur");
-	document.getElementById("logo").classList.toggle("blur");
+function onClickMenu() {
+	document.getElementById('menu').classList.toggle('change');
+	document.getElementById('nav').classList.toggle('change');
+	document.getElementById('menu-bg').classList.toggle('change-bg');
+	document.getElementById('content').classList.toggle('blur');
+	document.getElementById('logo').classList.toggle('blur');
 }
 
-function onClickBtn1(){
-	document.getElementById("project-bg").classList.toggle("project-change");
-	document.getElementById("title").classList.toggle("blur");
-	document.getElementById("circles-grid").classList.toggle("blur");
-	document.getElementById("footer").classList.toggle("blur");
-	document.getElementById("project2").classList.toggle("blur");
-	document.getElementById("project3").classList.toggle("blur");
-	document.getElementById("btn1").classList.toggle("blur");
-	document.getElementById("btn2").classList.toggle("blur");
-	document.getElementById("btn3").classList.toggle("blur");
-	document.getElementById("logo").classList.toggle("blur");
-	document.getElementById("menu").classList.toggle("blur");
-	document.getElementById("project-bg-2").classList.add("hide");
-	document.getElementById("project-bg-3").classList.add("hide");
-
+function onClickBtn1() {
+	document.getElementById('project-bg').classList.toggle('project-change');
+	document.getElementById('title').classList.toggle('blur');
+	document.getElementById('circles-grid').classList.toggle('blur');
+	document.getElementById('footer').classList.toggle('blur');
+	document.getElementById('project2').classList.toggle('blur');
+	document.getElementById('project3').classList.toggle('blur');
+	document.getElementById('btn1').classList.toggle('blur');
+	document.getElementById('btn2').classList.toggle('blur');
+	document.getElementById('btn3').classList.toggle('blur');
+	document.getElementById('logo').classList.toggle('blur');
+	document.getElementById('menu').classList.toggle('blur');
+	document.getElementById('project-bg-2').classList.add('hide');
+	document.getElementById('project-bg-3').classList.add('hide');
 }
 
-function onCloseBtn1(){
-	document.getElementById("project-bg").classList.toggle("project-change");
-	document.getElementById("title").classList.toggle("blur");
-	document.getElementById("circles-grid").classList.toggle("blur");
-	document.getElementById("footer").classList.toggle("blur");
-	document.getElementById("project2").classList.toggle("blur");
-	document.getElementById("project3").classList.toggle("blur");
-	document.getElementById("btn1").classList.toggle("blur");
-	document.getElementById("btn2").classList.toggle("blur");
-	document.getElementById("btn3").classList.toggle("blur");
-	document.getElementById("logo").classList.toggle("blur");
-	document.getElementById("menu").classList.toggle("blur");
-	
+function onCloseBtn1() {
+	document.getElementById('project-bg').classList.toggle('project-change');
+	document.getElementById('title').classList.toggle('blur');
+	document.getElementById('circles-grid').classList.toggle('blur');
+	document.getElementById('footer').classList.toggle('blur');
+	document.getElementById('project2').classList.toggle('blur');
+	document.getElementById('project3').classList.toggle('blur');
+	document.getElementById('btn1').classList.toggle('blur');
+	document.getElementById('btn2').classList.toggle('blur');
+	document.getElementById('btn3').classList.toggle('blur');
+	document.getElementById('logo').classList.toggle('blur');
+	document.getElementById('menu').classList.toggle('blur');
 }
 
-function onClickBtn2(){
-	document.getElementById("project-bg-2").classList.toggle("project-change");
-	document.getElementById("title").classList.toggle("blur");
-	document.getElementById("circles-grid").classList.toggle("blur");
-	document.getElementById("footer").classList.toggle("blur");
-	document.getElementById("project1").classList.toggle("blur");
-	document.getElementById("project3").classList.toggle("blur");
-	document.getElementById("btn1").classList.toggle("blur");
-	document.getElementById("btn2").classList.toggle("blur");
-	document.getElementById("btn3").classList.toggle("blur");
-	document.getElementById("logo").classList.toggle("blur");
-	document.getElementById("menu").classList.toggle("blur");
-	document.getElementById("project-bg").classList.add("hide");
-	document.getElementById("project-bg-3").classList.add("hide");
+function onClickBtn2() {
+	document.getElementById('project-bg-2').classList.toggle('project-change');
+	document.getElementById('title').classList.toggle('blur');
+	document.getElementById('circles-grid').classList.toggle('blur');
+	document.getElementById('footer').classList.toggle('blur');
+	document.getElementById('project1').classList.toggle('blur');
+	document.getElementById('project3').classList.toggle('blur');
+	document.getElementById('btn1').classList.toggle('blur');
+	document.getElementById('btn2').classList.toggle('blur');
+	document.getElementById('btn3').classList.toggle('blur');
+	document.getElementById('logo').classList.toggle('blur');
+	document.getElementById('menu').classList.toggle('blur');
+	document.getElementById('project-bg').classList.add('hide');
+	document.getElementById('project-bg-3').classList.add('hide');
 }
 
-function onCloseBtn2(){
-	document.getElementById("project-bg-2").classList.toggle("project-change");
-	document.getElementById("title").classList.toggle("blur");
-	document.getElementById("circles-grid").classList.toggle("blur");
-	document.getElementById("footer").classList.toggle("blur");
-	document.getElementById("project1").classList.toggle("blur");
-	document.getElementById("project3").classList.toggle("blur");
-	document.getElementById("btn1").classList.toggle("blur");
-	document.getElementById("btn2").classList.toggle("blur");
-	document.getElementById("btn3").classList.toggle("blur");
-	document.getElementById("logo").classList.toggle("blur");
-	document.getElementById("menu").classList.toggle("blur");
+function onCloseBtn2() {
+	document.getElementById('project-bg-2').classList.toggle('project-change');
+	document.getElementById('title').classList.toggle('blur');
+	document.getElementById('circles-grid').classList.toggle('blur');
+	document.getElementById('footer').classList.toggle('blur');
+	document.getElementById('project1').classList.toggle('blur');
+	document.getElementById('project3').classList.toggle('blur');
+	document.getElementById('btn1').classList.toggle('blur');
+	document.getElementById('btn2').classList.toggle('blur');
+	document.getElementById('btn3').classList.toggle('blur');
+	document.getElementById('logo').classList.toggle('blur');
+	document.getElementById('menu').classList.toggle('blur');
 }
 
-function onClickBtn3(){
-	document.getElementById("btn3").classList.toggle("change");
-	document.getElementById("project-bg-3").classList.toggle("project-change");
-	document.getElementById("title").classList.toggle("blur");
-	document.getElementById("circles-grid").classList.toggle("blur");
-	document.getElementById("footer").classList.toggle("blur");
-	document.getElementById("project2").classList.toggle("blur");
-	document.getElementById("project1").classList.toggle("blur");
-	document.getElementById("btn1").classList.toggle("blur");
-	document.getElementById("btn2").classList.toggle("blur");
-	document.getElementById("btn3").classList.toggle("blur");
-	document.getElementById("logo").classList.toggle("blur");
-	document.getElementById("menu").classList.toggle("blur");
-	document.getElementById("project-bg").classList.add("hide");
-	document.getElementById("project-bg-2").classList.add("hide");
+function onClickBtn3() {
+	document.getElementById('btn3').classList.toggle('change');
+	document.getElementById('project-bg-3').classList.toggle('project-change');
+	document.getElementById('title').classList.toggle('blur');
+	document.getElementById('circles-grid').classList.toggle('blur');
+	document.getElementById('footer').classList.toggle('blur');
+	document.getElementById('project2').classList.toggle('blur');
+	document.getElementById('project1').classList.toggle('blur');
+	document.getElementById('btn1').classList.toggle('blur');
+	document.getElementById('btn2').classList.toggle('blur');
+	document.getElementById('btn3').classList.toggle('blur');
+	document.getElementById('logo').classList.toggle('blur');
+	document.getElementById('menu').classList.toggle('blur');
+	document.getElementById('project-bg').classList.add('hide');
+	document.getElementById('project-bg-2').classList.add('hide');
 }
 
-function onCloseBtn3(){
-	document.getElementById("btn3").classList.toggle("change");
-	document.getElementById("project-bg-3").classList.toggle("project-change");
-	document.getElementById("title").classList.toggle("blur");
-	document.getElementById("circles-grid").classList.toggle("blur");
-	document.getElementById("footer").classList.toggle("blur");
-	document.getElementById("project2").classList.toggle("blur");
-	document.getElementById("project1").classList.toggle("blur");
-	document.getElementById("btn1").classList.toggle("blur");
-	document.getElementById("btn2").classList.toggle("blur");
-	document.getElementById("btn3").classList.toggle("blur");
-	document.getElementById("logo").classList.toggle("blur");
-	document.getElementById("menu").classList.toggle("blur");
+function onCloseBtn3() {
+	document.getElementById('btn3').classList.toggle('change');
+	document.getElementById('project-bg-3').classList.toggle('project-change');
+	document.getElementById('title').classList.toggle('blur');
+	document.getElementById('circles-grid').classList.toggle('blur');
+	document.getElementById('footer').classList.toggle('blur');
+	document.getElementById('project2').classList.toggle('blur');
+	document.getElementById('project1').classList.toggle('blur');
+	document.getElementById('btn1').classList.toggle('blur');
+	document.getElementById('btn2').classList.toggle('blur');
+	document.getElementById('btn3').classList.toggle('blur');
+	document.getElementById('logo').classList.toggle('blur');
+	document.getElementById('menu').classList.toggle('blur');
 }
